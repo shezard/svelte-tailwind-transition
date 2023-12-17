633b48dc52e40b47229026cd3acb27ae6ec80d52
unknown <dam.fayol@gmail.com>
2023-12-17 15:17:56 +0100

    Add basic working code for tailwind transition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,47 @@
 <script lang="ts">
-    console.log('test');
+    import transition from '$lib/tailwind-transition';
+
+    const card = transition({
+        base: 'flex border border-black rounded justify-center',
+        variants: {
+            size: {
+                small: 'w-20',
+                big: 'w-40 p-4',
+            },
+            color: {
+                red: 'bg-red-500',
+                green: 'bg-green-500',
+            }
+        },
+        transitions: {
+            size: {
+                from: 'small',
+                to: 'big',
+                class: 'transition-all duration-1000'
+            },
+        }
+    });
+
+    let state : 'big' | 'small' = 'small';
+
+    function toggleState() {
+        if(state == 'small') {
+            state = 'big'
+        } else {
+            state = 'small';
+        }
+    }
+
+
 </script>
 
-<h1 class="text-xl font-bold underline">
-    Hello world!
-</h1>
+<div class="container mx-auto">
+    <div class="flex flex-col p-4 gap-4">
+        <button class={card()}>
+            Content
+        </button>
+        <button class={card({ size: state, color: 'green'})} on:click={toggleState}>
+            Content 2
+        </button>
+    </div>
+</div>
